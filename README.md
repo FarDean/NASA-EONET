faebe0dbc0913cbceb2dcd3ad9505569003573d0
FarDean <sfh1212@gmail.com>
2021-02-23 16:12:54 +0330

    deployed

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # NASA AND GOOGLE MAP PROJECT
 
 This project was made by help of Traversy media!
+
+## Deployed at:
+
+[here](panoramic-size.surge.sh)
--- src/App.js
+++ src/App.js
@@ -2,22 +2,21 @@ import Map from "./components/Map";
 import { useEffect, useState } from "react";
 function App() {
 	const [data, setData] = useState([]);
+	const [loading, setLoading] = useState(false);
 
 	useEffect(() => {
 		async function fetchData() {
+			setLoading(true);
 			const res = await fetch("https://eonet.sci.gsfc.nasa.gov/api/v2.1/events");
 			const data = await res.json();
 			setData(data.events);
+			setLoading(false);
 		}
 
 		fetchData();
 	}, []);
 
-	return (
-		<>
-			<Map data={data} />
-		</>
-	);
+	return <>{loading ? <h1>Loading ...</h1> : <Map data={data} />}</>;
 }
 
 export default App;
